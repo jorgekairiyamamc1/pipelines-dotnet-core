07b33701c1fec194780add645ea5a4eea136b0cc
jorgekairiyamamc1 <jorge.kairiyama@contractor-mc1.com>
2024-07-12 16:59:27 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+/*comment here*/
 namespace pipelines_dotnet_core
 {
     public class Startup
